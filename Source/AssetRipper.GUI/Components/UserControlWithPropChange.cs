a6709a6a224c7d080e79dd32b73db9e183f14223
102138304+ShiyumeMeguri@users.noreply.github.com <102138304+ShiyumeMeguri@users.noreply.github.com>
2023-12-29 19:52:20 +0800

    Initial 20021442be0d0b30b6782f81bd728735f9f375e6

---
+++ Source/AssetRipper.GUI/Components/UserControlWithPropChange.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AssetRipper.GUI.Components
+{
+	public abstract class UserControlWithPropChange : UserControl, INotifyPropertyChanged
+	{
+		public new event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new(propertyName));
+		}
+	}
+}
