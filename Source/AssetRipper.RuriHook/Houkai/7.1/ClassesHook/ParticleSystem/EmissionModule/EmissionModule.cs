5a18363214c61c4842ea583d1a43bda9a0a242e4
102138304+ShiyumeMeguri@users.noreply.github.com <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-01-03 09:35:06 +0800

    HK 7.1 ParticleSystem

---
+++ Source/AssetRipper.RuriHook/Houkai/7.1/ClassesHook/ParticleSystem/EmissionModule/EmissionModule.cs
@@ -0,0 +1,26 @@
+﻿using AssetRipper.IO.Endian;
+using AssetRipper.SourceGenerated.Subclasses.EmissionModule;
+using AssetRipper.SourceGenerated.Subclasses.ParticleSystemEmissionBurst;
+using AssetRipper.SourceGenerated.Subclasses.Vector4f;
+
+namespace AssetRipper.RuriHook.Houkai_7_1;
+public partial class Houkai_7_1_Hook
+{
+	[RetargetMethod(typeof(EmissionModule_2017_3_0))]
+	public void EmissionModule_2017_3_0_ReadRelease(ref EndianSpanReader reader)
+	{
+		var _this = (object)this as EmissionModule_2017_3_0;
+		var type = typeof(EmissionModule_2017_3_0);
+
+		_this.Enabled = reader.ReadRelease_BooleanAlign();
+		_this.RateOverTime.ReadRelease(ref reader);
+		_this.RateOverDistance.ReadRelease(ref reader);
+		_this.BurstCount_Int32 = reader.ReadRelease_Int32Align();
+		SetAssetListField<ParticleSystemEmissionBurst_2017_3_0>(type, "m_Bursts", ref reader);
+		Vector4f m_EmissionLevel = new();
+		m_EmissionLevel.ReadRelease(ref reader);
+		var m_EmissionFalloffStart = reader.ReadSingle();
+		var m_EmissionFalloffEnd = reader.ReadSingle();
+		var m_EnableFallOff = reader.ReadRelease_BooleanAlign();
+	}
+}
---
+++ Source/AssetRipper.RuriHook/Houkai/7.1/ClassesHook/ParticleSystem/ParticleSystem.cs
@@ -0,0 +1,56 @@
+﻿using AssetRipper.IO.Endian;
+using AssetRipper.RuriHook.Houkai_7_1.Classes.Subclasses.TextModule;
+using AssetRipper.SourceGenerated.Classes.ClassID_198;
+using AssetRipper.SourceGenerated.Subclasses.MinMaxCurve;
+
+namespace AssetRipper.RuriHook.Houkai_7_1;
+public partial class Houkai_7_1_Hook
+{
+	[RetargetMethod(typeof(ParticleSystem_2017_3_0))]
+	public void ParticleSystem_2017_3_0_ReadRelease(ref EndianSpanReader reader)
+	{
+		var _this = (object)this as ParticleSystem_2017_3_0;
+		var type = typeof(ParticleSystem_2017_3_0);
+
+		_this.GameObject.ReadRelease(ref reader);
+		_this.LengthInSec = reader.ReadSingle();
+		_this.SimulationSpeed = reader.ReadSingle();
+		_this.StopAction = reader.ReadInt32();
+		_this.CullingMode = reader.ReadInt32();
+		_this.Looping = reader.ReadBoolean();
+		_this.Prewarm = reader.ReadBoolean();
+		_this.PlayOnAwake = reader.ReadBoolean();
+		_this.UseUnscaledTime = reader.ReadBoolean();
+		_this.AutoRandomSeed = reader.ReadBoolean();
+		_this.UseRigidbodyForVelocity = reader.ReadRelease_BooleanAlign();
+		((MinMaxCurve_2017_1_0_b3)_this.StartDelay_MinMaxCurve).ReadRelease_AssetAlign(ref reader);
+		_this.MoveWithTransform_Int32 = reader.ReadRelease_Int32Align();
+		_this.MoveWithCustomTransform.ReadRelease(ref reader);
+		_this.ScalingMode = reader.ReadInt32();
+		_this.RandomSeed_Int32 = reader.ReadInt32();
+		_this.InitialModule.ReadRelease(ref reader);
+		_this.ShapeModule.ReadRelease(ref reader);
+		_this.EmissionModule.ReadRelease(ref reader);
+		_this.SizeModule.ReadRelease(ref reader);
+		_this.RotationModule.ReadRelease(ref reader);
+		_this.ColorModule.ReadRelease(ref reader);
+		_this.UVModule.ReadRelease(ref reader);
+		_this.VelocityModule.ReadRelease(ref reader);
+		_this.InheritVelocityModule.ReadRelease(ref reader);
+		_this.ForceModule.ReadRelease(ref reader);
+		_this.ExternalForcesModule.ReadRelease(ref reader);
+		_this.ClampVelocityModule.ReadRelease(ref reader);
+		_this.NoiseModule.ReadRelease(ref reader);
+		_this.SizeBySpeedModule.ReadRelease(ref reader);
+		_this.RotationBySpeedModule.ReadRelease(ref reader);
+		_this.ColorBySpeedModule.ReadRelease(ref reader);
+		_this.CollisionModule.ReadRelease(ref reader);
+		_this.TriggerModule.ReadRelease(ref reader);
+		_this.SubModule.ReadRelease(ref reader);
+		_this.LightsModule.ReadRelease(ref reader);
+		_this.TrailModule.ReadRelease(ref reader);
+		_this.CustomDataModule.ReadRelease(ref reader);
+		TextModule textModule = new();
+		textModule.ReadRelease(ref reader);
+	}
+}
