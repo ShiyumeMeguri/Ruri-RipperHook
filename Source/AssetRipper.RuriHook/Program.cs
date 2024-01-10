146ce127f5d909c453b88582db24e8af3322aa0f
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-01-10 12:34:51 +0900

    Change to application

---
+++ Source/AssetRipper.RuriHook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace AssetRipper.RuriHook;
+
+static class Program
+{
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		RuriRuntimeHook.Init(GameHookType.ShaderDecompiler);
+		Type programType = Type.GetType("AssetRipper.GUI.Program, AssetRipper");
+		MethodInfo mainMethod = programType.GetMethod("Main", ReflectionExtension.AnyBindFlag());
+		object[] parameters = new object[] { args };
+		mainMethod.Invoke(null, parameters);
+	}
+}
