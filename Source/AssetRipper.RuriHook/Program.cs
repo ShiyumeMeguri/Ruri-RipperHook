3a491dad719903bab3e3a2a9b781db301102b02e
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-01-19 09:05:51 +0800

    Update Program.cs

--- Source/AssetRipper.RuriHook/Program.cs
+++ Source/AssetRipper.RuriHook/Program.cs
@@ -7,7 +7,16 @@ static class Program
 	[STAThread]
 	public static void Main(string[] args)
 	{
+		Hook();
+		RunAssetRipper(args);
+	}
+	private static void Hook()
+	{
 		RuriRuntimeHook.Init(GameHookType.ShaderDecompiler);
+		RuriRuntimeHook.Init(GameHookType.Houkai_7_1);
+	}
+	private static void RunAssetRipper(string[] args)
+	{
 		Type programType = Type.GetType("AssetRipper.GUI.Program, AssetRipper");
 		MethodInfo mainMethod = programType.GetMethod("Main", ReflectionExtension.AnyBindFlag());
 		object[] parameters = new object[] { args };
