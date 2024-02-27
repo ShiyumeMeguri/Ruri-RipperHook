05867b64495bb1c8f6cc125f273335fe99718f55
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-02-27 16:13:38 +0900

    Clean code

--- Source/Ruri.RipperHook/Game/UnityCN/PunishingGrayRaven/2.11/PunishingGrayRaven_2_11_Hook.cs
+++ Source/Ruri.RipperHook/Game/UnityCN/PunishingGrayRaven/2.11/PunishingGrayRaven_2_11_Hook.cs
@@ -6,7 +6,6 @@ public partial class PunishingGrayRaven_2_11_Hook : AssetHook
 {
     protected PunishingGrayRaven_2_11_Hook()
     {
-        RuriRuntimeHook.gameCrypto = new();
         UnityCNCommon_Hook.SetKey("PGR CN/JP/TW", "7935585076714C4F72436F6B57524961");
     }
 
