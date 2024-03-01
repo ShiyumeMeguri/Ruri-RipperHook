61fde3c1ccf8f704e2b5009bba63166c66712efa
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-03-01 14:19:50 +0900

    CommonHook Comment

--- Source/Ruri.RipperHook/Utils/Hook/CommonHook.cs
+++ Source/Ruri.RipperHook/Utils/Hook/CommonHook.cs
@@ -1,5 +1,7 @@
 ﻿namespace Ruri.RipperHook.HookUtils;
-
+/// <summary>
+/// 这个类的目的是把一些非常常用的Hook简单化 避免重复代码 同时也可以避免更新导致的重复修改
+/// </summary>
 public class CommonHook
 {
     protected void SetPrivateField(Type type, string name, object newValue)
