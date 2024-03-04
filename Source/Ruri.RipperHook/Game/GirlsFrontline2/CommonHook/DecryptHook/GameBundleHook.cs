80b5fc9f12bfd52b9cac3ec3a6e8ba41c145a36f
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-03-04 17:05:32 +0900

    更好的识别少前文件

--- Source/Ruri.RipperHook/Game/GirlsFrontline2/CommonHook/DecryptHook/GameBundleHook.cs
+++ Source/Ruri.RipperHook/Game/GirlsFrontline2/CommonHook/DecryptHook/GameBundleHook.cs
@@ -12,6 +12,7 @@ public partial class GirlsFrontline2Common_Hook
     {
         foreach (var path in paths)
         {
+            var extension = Path.GetExtension(path);
             using (var stream = SmartStream.OpenRead(path))
             {
                 var fileData = new byte[stream.Length];
@@ -19,8 +20,11 @@ public partial class GirlsFrontline2Common_Hook
 
                 if (fileData.Length >= 4 && fileData[0] == 0x47 && fileData[1] == 0x46 && fileData[2] == 0x46 && fileData[3] == 0x00)
                     continue; // 文件头 "GFF" 这应该是索引文件
+                
+                if (extension == ".bundle")
+                    if (fileData.Length >= 8 && fileData[0] != 0x55 && fileData[1] != 0x6E && fileData[2] != 0x69 && fileData[3] != 0x74 && fileData[4] != 0x79 && fileData[5] != 0x46 && fileData[6] != 0x53 && fileData[7] != 0x00) // UnityFS
+                        fileData = RuriRuntimeHook.commonDecryptor.Decrypt(fileData).ToArray(); // 解密处
 
-                fileData = RuriRuntimeHook.commonDecryptor.Decrypt(fileData).ToArray(); // 解密处
                 fileStack.AddRange(GameBundleHook.LoadFilesAndDependencies(fileData, MultiFileStream.GetFilePath(path), MultiFileStream.GetFileName(path), dependencyProvider));
             }
         }
---
+++ Source/Ruri.RipperHook/Game/GirlsFrontline2/CommonHook/DecryptHook/ImportSettingsHook.cs
@@ -0,0 +1,27 @@
+using AssetRipper.Import.Configuration;
+using AssetRipper.Primitives;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace Ruri.RipperHook.GirlsFrontline2Common;
+
+public static class ImportSettingsHook
+{
+    [RetargetMethodCtorFunc(typeof(ImportSettings))]
+    public static bool Ctor(ILContext il)
+    {
+        var ilCursor = new ILCursor(il);
+        while (ilCursor.TryGotoNext(MoveType.After, instr => instr.OpCode == OpCodes.Ret)) ;
+        ilCursor.GotoPrev();
+        ilCursor.Emit(OpCodes.Ldarg_0);
+        ilCursor.Emit(OpCodes.Ldc_I4, 2019);
+        ilCursor.Emit(OpCodes.Ldc_I4, 4);
+        ilCursor.Emit(OpCodes.Ldc_I4, 40);
+        ilCursor.Emit(OpCodes.Ldc_I4, 3);
+        ilCursor.Emit(OpCodes.Ldc_I4, 1);
+        ilCursor.Emit(OpCodes.Newobj, il.Import(typeof(UnityVersion).GetConstructor(new Type[] { typeof(ushort), typeof(ushort), typeof(ushort), typeof(UnityVersionType), typeof(byte) })));
+        var destMethod = typeof(ImportSettings).GetMethod("set_DefaultVersion");
+        ilCursor.Emit(OpCodes.Call, destMethod);
+        return true;
+    }
+}
