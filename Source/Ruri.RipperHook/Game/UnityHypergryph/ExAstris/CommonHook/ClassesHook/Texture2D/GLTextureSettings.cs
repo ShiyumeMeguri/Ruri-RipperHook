b9b040c01d6e5d73340f52055843441f4b3b5d35
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-03-05 13:28:08 +0900

    ExAstris: Texture2D

---
+++ Source/Ruri.RipperHook/Game/UnityHypergryph/ExAstris/CommonHook/ClassesHook/Texture2D/GLTextureSettings.cs
@@ -0,0 +1,22 @@
+﻿using AssetRipper.IO.Endian;
+using AssetRipper.SourceGenerated.Subclasses.GLTextureSettings;
+
+namespace Ruri.RipperHook.ExAstrisCommon;
+
+public partial class ExAstrisCommon_Hook
+{
+    [RetargetMethod(typeof(GLTextureSettings_2017))]
+    public void Texture2D_2020_2_0_a16_ReadRelease(ref EndianSpanReader reader)
+    {
+        var _this = (object)this as GLTextureSettings_2017;
+        var type = typeof(GLTextureSettings_2017);
+
+        _this.FilterMode = reader.ReadInt32();
+        _this.Aniso = reader.ReadInt32();
+        _this.MipBias = reader.ReadSingle();
+        var m_TextureGroup = reader.ReadInt32();
+        _this.WrapU = reader.ReadInt32();
+        _this.WrapV = reader.ReadInt32();
+        _this.WrapW = reader.ReadInt32();
+    }
+}
