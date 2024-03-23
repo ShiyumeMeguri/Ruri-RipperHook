a329c3a1a74aac41e1a6a785db58b9650239454a
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-03-23 08:29:42 +0800

    更好的转换类型树dump

--- Source/Ruri.RipperHook/PyTools/TypeTreeConverter.py
+++ Source/Ruri.RipperHook/PyTools/TypeTreeConverter.py
@@ -1,14 +1,13 @@
+import sys
 import json
 import os
+import glob  # 用于找到匹配的文件路径
 
 def parse_node(node, file, level=0):
-    # 根据层级添加制表符
     indent = '\t' * level
-    # 检查MetaFlag以确定是否添加 // Align 注释
     align_comment = " // Align" if node["MetaFlag"] & 0x4000 else ""
     file.write(f"{indent}{node['TypeName']} {node['Name']};{align_comment}\n")
 
-    # 递归解析子节点
     if "SubNodes" in node:
         for subnode in node["SubNodes"]:
             parse_node(subnode, file, level + 1)
@@ -18,22 +17,22 @@ def parse_classes(classes, file):
         file.write(f"Class {cls['Name']}\n")
         if "ReleaseRootNode" in cls and cls["ReleaseRootNode"]:
             parse_node(cls["ReleaseRootNode"], file)
-        file.write("\n")  # 每个类别后添加一个空行
+        file.write("\n")
 
 def parse_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
 
     if "Classes" in data:
-        # 创建输出文件
-        output_file_name = f"{os.path.splitext(json_file)[0]}.txt"
+        # 修改输出文件路径到TypeTreeOutput文件夹
+        output_dir = "TypeTreeOutput"
+        os.makedirs(output_dir, exist_ok=True)  # 确保输出目录存在
+        output_file_name = os.path.join(output_dir, f"{os.path.splitext(os.path.basename(json_file))[0]}.txt")
         with open(output_file_name, 'w') as output_file:
             parse_classes(data["Classes"], output_file)
         print(f"Output written to {output_file_name}")
 
 if __name__ == "__main__":
-    json_file = 'info.json'
-    if len(sys.argv) > 1:
-        json_file = sys.argv[1]
-
-    parse_json(json_file)
+    # 遍历TypeTreeDump目录下的所有json文件
+    for json_file in glob.glob('TypeTreeDump/*.json'):
+        parse_json(json_file)
