aec2109a5d813d8330d2c98c24829557b2f59246
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-03-26 11:45:27 +0900

    SR Animator

---
+++ Source/Ruri.RipperHook/Game/UnityMihoyo/StarRail/2.0/ClassesHook/Animator/Animator.cs
@@ -0,0 +1,27 @@
+﻿using AssetRipper.IO.Endian;
+using AssetRipper.SourceGenerated.Classes.ClassID_95;
+
+namespace Ruri.RipperHook.StarRail_2_0;
+
+public partial class StarRail_2_0_Hook
+{
+    [RetargetMethod(typeof(Animator_2018_3))]
+    public void Animator_2018_3_ReadRelease(ref EndianSpanReader reader)
+    {
+        var _this = (object)this as Animator_2018_3;
+        var type = typeof(Animator_2018_3);
+
+        _this.GameObject.ReadRelease(ref reader);
+        _this.Enabled =  reader.ReadRelease_ByteAlign();
+        _this.Avatar.ReadRelease(ref reader);
+        _this.Controller_PPtr_RuntimeAnimatorController_5.ReadRelease(ref reader);
+        _this.CullingMode = reader.ReadInt32();
+        _this.UpdateMode = reader.ReadInt32();
+        var m_MotionSkeletonMode = reader.ReadInt32();
+        _this.ApplyRootMotion = reader.ReadBoolean();
+        _this.LinearVelocityBlending =  reader.ReadRelease_BooleanAlign();
+        _this.HasTransformHierarchy = reader.ReadBoolean();
+        _this.AllowConstantClipSamplingOptimization = reader.ReadBoolean();
+        _this.KeepAnimatorControllerStateOnDisable =  reader.ReadRelease_BooleanAlign();
+    }
+}
